ec952888a9f81167d40f251e7ce02e5603d76806
Nikema Prophet <nikema@gmail.com>
2016-05-08 01:41:30 -0700

    Main iPhone screen complete

---
+++ sacramento-favorite-place/RoundedBtn.swift
@@ -0,0 +1,18 @@
+//
+//  RoundedBtn.swift
+//  sacramento-favorite-place
+//
+//  Created by Nikema Prophet on 5/8/16.
+//  Copyright © 2016 Nikema Prophet. All rights reserved.
+//
+
+import UIKit
+
+class RoundedBtn: UIButton {
+
+    override func awakeFromNib() {
+        self.layer.cornerRadius = 3.0
+
+    }
+
+}
